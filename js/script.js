17db69a6ac346453f67a95706ade99404e2eb115
Aya Hamadani <ayahamadani2004@gmail.com>
2025-06-05 22:00:30 +0300

    Remove all special characters from the string input.

--- js/script.js
+++ js/script.js
@@ -6,7 +6,13 @@ const copyButton = document.getElementById('copy-button');
 nameButton.addEventListener("click", () => {
     const textToRename = input.value;
     if(textToRename){
-        namingOutput.textContent = textToRename.toLowerCase().replace(/\s+/g, '-');
+        namingOutput.textContent = textToRename
+        .toLowerCase()
+        .replace(/[\s_]+/g, '-')                          
+        .replace(/['"`‘’“”]/g, '')    
+        .replace(/[^a-z0-9\-]/g, '') 
+        .replace(/\-+/g, '-')
+        .replace(/^-+|-+$/g, '');      ;
         copyButton.style.display = "block";
         namingOutput.style.display = "block";
     }
